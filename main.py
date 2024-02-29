2b2f7b3518d529cae5b38e986b0a03e855db8979
Kenref <107287396+Kenref@users.noreply.github.com>
2024-02-29 18:39:46 +1100

    created target class

--- main.py
+++ main.py
@@ -10,6 +10,35 @@ WIDTH, HEIGHT = 800, 600
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Aim Trainer")
 
+class Target:
+  MAX_SIZE = 30
+  GROWTH_RATE = 0.2
+  COLOUR = "red"
+
+  def __init__(self,x,y):
+    self.x = x
+    self.y = y
+    self.size = 0
+    self.grow = True
+  
+  def update(self):
+    if self.size + self.GROWTH_RATE >= self.MAX_SIZE:
+      self.grow = False
+
+    if self.grow:
+      self.size += self.GROWTH_RATE
+    else:
+      self.size -= self.GROWTH_RATE
+
+    
+
+
+
+
+
+
+
+
 def main():
   run = True
 
